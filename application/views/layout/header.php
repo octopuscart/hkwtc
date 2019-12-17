69ad69e49550c1dd3b3b4d552a5d2765b01972ef
dev <dev@develop.com>
2019-12-17 15:11:11 +0530

    changes

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -100,7 +100,7 @@
                                 <li><a href="<?php echo site_url("Product/ProductList/2/0");?>" ><div>Suits</div></a></li>
                                 <li><a href="#" data-href="#section-testimonials"><div>Shirts</div></a></li>
                                  <li><a href="#" data-href="#section-testimonials"><div>Lining </div></a></li> <li><a href="#" data-href="#section-about"><div>About</div></a></li>
-                                <li><a href="<?php echo site_url("Shop/contactus");?>" data-href="#section-contact"><div>Contact</div></a></li>
+                                <li><a href="<?php echo site_url("Shop/contactus");?>" ><div>Contact</div></a></li>
 
                             </ul>
 
