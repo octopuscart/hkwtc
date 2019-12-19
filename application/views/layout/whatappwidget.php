add3d7008f313ce89dbc963a555f6934b2c73649
dev <dev@develop.com>
2019-12-19 13:46:47 +0530

    changes

--- application/views/layout/whatappwidget.php
+++ application/views/layout/whatappwidget.php
@@ -2,6 +2,11 @@
 <div class="elfsight-app-d016b79c-5e29-4bc0-a3b0-d7ffbb987c49"></div>
 
 <script>
+    
+  
+       
+  
+    
 setTimeout(function(){
     $(".eapps-widget-show-toolbar a[role='button']").next().find("a").remove();
 },3000)
